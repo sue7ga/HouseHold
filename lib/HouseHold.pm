48eb98382515c246d029d074602ec09862bbf4f9
sue7ga <sue77ga@gmail.com>
2014-12-11 14:50:47 -0500

    initial import

---
+++ lib/HouseHold.pm
@@ -0,0 +1,47 @@
+package HouseHold;
+use strict;
+use warnings;
+use utf8;
+our $VERSION='0.01';
+use 5.008001;
+use HouseHold::DB::Schema;
+use HouseHold::DB;
+
+use parent qw/Amon2/;
+# Enable project local mode.
+__PACKAGE__->make_local_context();
+
+my $schema = HouseHold::DB::Schema->instance;
+
+sub db {
+    my $c = shift;
+    if (!exists $c->{db}) {
+        my $conf = $c->config->{DBI}
+            or die "Missing configuration about DBI";
+        $c->{db} = HouseHold::DB->new(
+            schema       => $schema,
+            connect_info => [@$conf],
+            # I suggest to enable following lines if you are using mysql.
+            # on_connect_do => [
+            #     'SET SESSION sql_mode=STRICT_TRANS_TABLES;',
+            # ],
+        );
+    }
+    $c->{db};
+}
+
+1;
+__END__
+
+=head1 NAME
+
+HouseHold - HouseHold
+
+=head1 DESCRIPTION
+
+This is a main context class for HouseHold
+
+=head1 AUTHOR
+
+HouseHold authors.
+
---
+++ t/00_compile.t
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+use HouseHold;
+use HouseHold::Web;
+use HouseHold::Web::View;
+use HouseHold::Web::ViewFunctions;
+
+use HouseHold::DB::Schema;
+use HouseHold::Web::Dispatcher;
+
+
+pass "All modules can load.";
+
+done_testing;
